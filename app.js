f9583844d257fb65277c174a2269c3e9beba3420
Raj <prmrraj.1911@gmail.com>
2026-03-02 23:14:41 +0530

    added teal styling and also bootstrap form validation

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const Listing = require("./models/listing.js");
+const ejsMate = require("ejs-mate");
+
 
 app.set("view engine","ejs");
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views",path.join(__dirname, "views"));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
 
+//middleware to show the currentPage 
+app.use((req,res,next)=>{
+    res.locals.currentPage = req.path;
+    next();
+})
 
-const port = 3000;
+
+const port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/Stayora";
 
 connectDB().catch(err => {
@@ -28,7 +38,7 @@ async function connectDB() {
 //index route
 
 app.get("/",(req,res)=>{
-    res.send("Root is working");
+    res.render("listings/home",{currentPage : "home"});
 })
 
 //all listings route 
